2c32cfbe6d7943c73057c23b901471d56277e1a7
HeseltineBTutu <heseltinetutu@gmail.com>
2023-02-13 02:33:52 -0800

    #issue 1: bug fix

--- simple_shell.c
+++ simple_shell.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
-int main(void)
+int main(int ac, char **argv)
 {
 	char *command = NULL;
 	size_t len = 0;
 	ssize_t read;
-	pid_t pid;
-	int status;
+	(void) ac; (void) argv;
 
 	while (1)
 	{
 		/* Display prompt*/
 		printf("$ ");
-		fflush(stdout);
 
 		/*Read command*/
 		read = getline(&command, &len, stdin);
@@ -21,33 +19,6 @@ int main(void)
 		{
 			printf("\n");
 			break;
-		}
-		/* Remove newline character from the command */
-		command[strcspn(command, "\n")] = 0;
-
-		/* Createa child process to execute the command*/
-		pid = fork();
-
-		if (pid == 0)
-		{
-			/* In child process */
-			char args[] = {command, NULL};
-			execve(command, args,  NULL);
-
-			/*if execv returns, it means the command was not found*/
-			perror("simple_shell: command not found");
-			exit(1);
-		}
-		else if (pid < 0)
-		{
-			/*Handle fork error */
-			perror("simple_shell: error forking");
-			continue;
-		}
-		else
-		{
-			/*I parent process, wait  for child to finish */
-			waitpid(pid, &status, 0);
 		}
 	}
 	/* Free memory allocated by getline */
