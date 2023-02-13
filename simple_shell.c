0fdf12b30d20c7aaf2c037394caa4a4dbaddffc0
Nataly-Aketch <nataliebeckie@gmail.com>
2023-02-13 15:10:24 +0300

    Bug fix

--- simple_shell.c
+++ simple_shell.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int main(int ac, char **argv)
+int main(/*int ac, char **argv*/)
 {
 	char *command = NULL;
 	size_t len = 0;
@@ -9,9 +9,11 @@ int main(int ac, char **argv)
 	const char *delim = "\n";
 	char *token;
 	int num_tokens = 0;
-	int i;
+	int i, status;
+	pid_t pid;
+	char *argv[] = {"", NULL};
 
-	(void) ac; (void) argv;
+	/*(void) ac; */
 	while (1)
 	{
 		/* Display prompt*/
@@ -19,14 +21,24 @@ int main(int ac, char **argv)
 
 		/*Read command*/
 		read = getline(&command, &len, stdin);
-		
+		token = strtok(command, delim);
 		if (read == -1)
 		{
 			printf("\n");
 			break;
 		}
+		pid = fork();
+		if (pid < 0)
+			perror("simple_shell");
+		if (pid == 0)
+		{
+			if ((execve(token, argv, NULL)) == -1)
+				perror("simple_shell");
+			exit(0);
+		}
+		waitpid(pid, &status, 0);
 	}
-	/* allocate space for a copy of the lineptr */
+	/* allocate space for a copy of the lineptr 
 
 	lineptr_copy = malloc(sizeof(char) * read);
 	if (lineptr_copy == NULL)
@@ -34,7 +46,7 @@ int main(int ac, char **argv)
 		perror("simple_shell:" );
 		return (-1);
 	}
-	/*copy lineptr to lineptr_copy*/
+	*copy lineptr to lineptr_copy
 	strcpy(lineptr_copy, lineptr);
 	token = strtok(lineptr, delim);
 
@@ -45,9 +57,9 @@ int main(int ac, char **argv)
 	}
 	num_tokens++;
 
-	/*Allocate space to hold the array of strings */
+	*Allocate space to hold the array of strings
 	argv = malloc(sizeof(char *) * num_tokens);
-	/*Store each token in argv array*/
+	*Store each token in argv array
 	token = strtok(lineptr_copy, delim);
 
 	for (i = 0; token != NULL; i++)
@@ -59,7 +71,7 @@ int main(int ac, char **argv)
 	argv[i] = NULL;
 
 	printf("%s\n", lineptr);
-	/* Free memory allocated by getline */
+	* Free memory allocated by getline */
 	free(command);
 	return (0);
 }
