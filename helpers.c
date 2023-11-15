6da47cd702e3cb14f2fe45926a9fd3162a2424d8
HeseltineBTutu <heseltinetutu@gmail.com>
2023-11-15 03:56:04 -0500

    removed trailing white space

--- helpers.c
+++ helpers.c
@@ -2,17 +2,17 @@
 
 /**
  * free_tokens - Free memory allocated for tokens
- * 
+ *
  * @tokens: The array of tokens to free
  * @token_count: The number of tokens in the array
 */
 void free_tokens(char **tokens, int token_count)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < token_count; i++)
-    {
-        free(tokens[i]);
-        tokens[i] = NULL;
-    }
+	for (i = 0; i < token_count; i++)
+	{
+		free(tokens[i]);
+		tokens[i] = NULL;
+	}
 }
